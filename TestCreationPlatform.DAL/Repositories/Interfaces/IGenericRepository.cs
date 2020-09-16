cc51891675751cac43059ac78e81778b3c928980
Tanya Khreshchykava <tanya.khreshchykava@gmail.com>
2020-09-16 01:14:16 -0400

    Added DAL

---
+++ TestCreationPlatform.DAL/Repositories/Interfaces/IGenericRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCreationPlatform.DAL.Repositories.Interfaces
+{
+    public interface IGenericRepository<TEntity> where TEntity : class
+    {
+        void Create(TEntity item);
+
+        void Delete(int id);
+
+        void Update(int id, TEntity item);
+
+        TEntity GetItem(int id);
+
+        IQueryable<TEntity> GetAll();
+
+    }
+}
