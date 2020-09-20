6fd874b17260cb6b0b5654ae23247d107b6af5f6
Tanya Khreshchykava <tanya.khreshchykava@gmail.com>
2020-09-19 20:41:16 -0400

    Changed relationships in DB to allow deleting, updated EDM

--- TestCreationPlatform.DAL/Data/Test.cs
+++ TestCreationPlatform.DAL/Data/Test.cs
@@ -18,6 +18,7 @@ namespace TestCreationPlatform.DAL.Data
         public Test()
         {
             this.Questions = new HashSet<Question>();
+            this.TestsResults = new HashSet<TestsResult>();
         }
     
         public int TestID { get; set; }
@@ -29,5 +30,7 @@ namespace TestCreationPlatform.DAL.Data
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Question> Questions { get; set; }
         public virtual Topic Topic { get; set; }
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<TestsResult> TestsResults { get; set; }
     }
 }
--- TestCreationPlatform.DAL/Data/TestsResult.cs
+++ TestCreationPlatform.DAL/Data/TestsResult.cs
@@ -20,5 +20,6 @@ namespace TestCreationPlatform.DAL.Data
         public int QuestionsTotal { get; set; }
     
         public virtual Topic Topic { get; set; }
+        public virtual Test Test { get; set; }
     }
 }
